cb82ce5144950903c7c77d22ec90632d7ea32eb7
Ephraim5 <111990405+Ephraim5@users.noreply.github.com>
2024-09-25 03:59:54 +0100

    Update server.js
    cors update

--- server.js
+++ server.js
@@ -6,7 +6,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(cors());
+const corsOptions = {
+  origin: ['https://todo-list-stack.onrender.com/','https://todo-list-stack.onrender.com/Home','https://todo-list-stack.onrender.com/view',], 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  maxAge: 3600, 
+};
+app.use(cors(corsOptions));
 connectToMongo()
 const PORT = process.env.PORT || 8000;
 
