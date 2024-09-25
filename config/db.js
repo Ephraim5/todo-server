035faf95cc634bac90da387d5086871758b2a703
Ephraim5 <ahitubephraim5@gmail.com>
2024-09-25 02:07:01 +0100

    Meaningful commit message

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 import mongoose  from "mongoose";
 const connectToMongo = async ()=>{
-    const res =  await mongoose.connect('mongodb://localhost:27017/todoDB')
+    const res =  await mongoose.connect(process.env.MONGO_URL)
     res && console.log('connected to MongoDB')
 }
 export default connectToMongo;
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import connectToMongo from './config/db.js';
 import cors from 'cors';
 import routes from './routes/TodoRoute.js'
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
